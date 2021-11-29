d8280c5725e898216f54b5cd58d7bceedfd777cf
crow <katcrow75@gmail.com>
2021-11-29 17:26:17 +0900

    'firstCommit'

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:graghql_flutter/Screens/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
